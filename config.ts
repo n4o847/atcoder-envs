d61fd824090465083d0e75b12c756f1b5380df72
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-07-03 22:37:51 +0900

    Add envs/4008, envs/4019

--- config.ts
+++ config.ts
@@ -1,8 +1,18 @@
 export const envs = [
   {
+    id: "4008",
+    name: "bc (1.07.1)",
+    ext: "bc",
+  },
+  {
     id: "4009",
     name: "Awk (GNU Awk 4.1.4)",
     ext: "awk",
+  },
+  {
+    id: "4019",
+    name: "dc (1.4.1)",
+    ext: "dc",
   },
   {
     id: "4049",
---
+++ envs/4008/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+bc --mathlib "$1"
